a2d849c05ba4463544740e8fb53b75ef809b691f
viktormokrecov <viktormokrecov@gmail.com>
2020-03-06 13:38:53 +0300

    init commit

---
+++ src/test/java/com/vmokrecov/apiservice/ApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmokrecov.apiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
