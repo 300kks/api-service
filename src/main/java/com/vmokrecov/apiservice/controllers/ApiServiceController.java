643c91382281a2478c536c8bd6ea880de14e32c8
viktormokrecov <viktormokrecov@gmail.com>
2020-03-09 09:38:19 +0300

    add api-service controller

---
+++ src/main/java/com/vmokrecov/apiservice/controllers/ApiServiceController.java
@@ -0,0 +1,14 @@
+package com.vmokrecov.apiservice.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class ApiServiceController {
+
+    @GetMapping("/")
+    public Mono<String> home() {
+        return Mono.just("{ \"message\": \"Home\" }");
+    }
+}
