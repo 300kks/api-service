33ee75f466143869b4eb0362a562105a0913e2fb
viktormokrecov <viktormokrecov@gmail.com>
2020-03-09 09:46:17 +0300

    add spring cloud config

---
+++ src/main/java/com/vmokrecov/apiservice/config/SpringCloudConfig.java
@@ -0,0 +1,24 @@
+package com.vmokrecov.apiservice.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringCloudConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/hello")
+                        .uri("lb://helloservice/hello/")
+                        .id("helloService"))
+
+                .route(r -> r.path("/world")
+                        .uri("http://worldservice:8082/world/")
+                        .id("worldService"))
+
+                .build();
+    }
+}
