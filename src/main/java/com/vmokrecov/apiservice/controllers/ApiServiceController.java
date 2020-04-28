5b8634264248e83657b34c4b9e2fe98b0d81b894
viktormokrecov <viktormokrecov@gmail.com>
2020-04-28 12:16:26 +0300

    add swagger to controller

--- src/main/java/com/vmokrecov/apiservice/controllers/ApiServiceController.java
+++ src/main/java/com/vmokrecov/apiservice/controllers/ApiServiceController.java
@@ -1,10 +1,12 @@
 package com.vmokrecov.apiservice.controllers;
 
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
+@Api(tags = "ApiServiceController admin", description = "ApiServiceController description")
 public class ApiServiceController {
 
     @GetMapping("/")
